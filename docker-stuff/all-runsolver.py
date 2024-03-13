41b9e134f6e52c2cd036a834a0861fd5182c1c77
geoffgeoffgeoff3 <2geoff@pobox.com>
2024-03-13 11:43:35 -0400

    close to generic run

--- docker-stuff/all-runsolver.py
+++ docker-stuff/all-runsolver.py
@@ -13,24 +13,26 @@ def getRunsolverArgs(args):
 
 def getRunscriptArgs(args):
     parts = {
-        'i': args.intent,
-        'b': "/artifacts/CWD/benchmark",
-        'c': args.wall_clock_limit,
+        'P': "/artifacts/CWD/benchmark",
+        'C': args.cpu_limit,
+        'W': args.wall_clock_limit,
+        'I': args.intent,
+        'M': args.memory_limit,
     }
-    return ' '.join([parts[c] for c in args.runscript_format])
+    return ' '.join([str(parts[c.upper()]) for c in args.runscript_args])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("A script that wraps a podman call to a prover image")
+    parser.add_argument("image_name", help="Image name e.g., eprover:3.0.03-runsolver-arm64")
+    parser.add_argument("runscript", help="The system script and its args, e.g., 'run_E PWI'")
+    # parser.add_argument("-f","--runscript_args", default="P", help="A string for the args (PCWI)")
     parser.add_argument("problem")
-    parser.add_argument("-c", "--cpu-limit", default=300, type=int, help="Max CPU time in seconds")
-    parser.add_argument("-w", "--wall-clock-limit", default=300, type=int, help="Max wall clock time in seconds")
-    parser.add_argument("-m", "--memory-limit", default=-1, type=int, help="Max memory usage in MB")
-    parser.add_argument("--intent", default="THM", choices=["THM", "SAT"], help="specify intent (THM, SAT, etc)")
+    parser.add_argument("-C", "--cpu-limit", default=60, type=int, help="Max CPU time in seconds")
+    parser.add_argument("-W", "--wall-clock-limit", default=60, type=int, help="Max wall clock time in seconds")
+    parser.add_argument("-M", "--memory-limit", default=-1, type=int, help="Max memory usage in MB")
+    parser.add_argument("-I", "--intent", default="THM", choices=["THM", "SAT"], help="specify intent (THM, SAT, etc)")
     parser.add_argument("--dry-run", action="store_true", help="dry run")
-    parser.add_argument("--image-name", default="eprover:3.0.03-runsolver", help="specify image name")
-    parser.add_argument("-r", "--runscript", default="run_E", help="The script runsolver runs")
-    parser.add_argument("-f","--runscript_format", default="")
     args = parser.parse_args()
 
     # Define podman command
