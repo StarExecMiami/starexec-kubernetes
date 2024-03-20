aebe1997af4e38ccb969c72c08125164d9192591
Jack McKeown <jackamckeown@gmail.com>
2024-03-20 10:49:37 -0400

    run_container in all-runsolver and reading from stdin if no problem given

--- docker-stuff/all-runsolver.py
+++ docker-stuff/all-runsolver.py
@@ -2,7 +2,7 @@
 
 import argparse
 import subprocess
-import os
+import os, sys
 import shutil
 
 
@@ -21,12 +21,19 @@ def getRunscriptArgs(args, args_format):
     }
     return ' '.join([str(parts[c.upper()]) for c in args_format])
 
+def makeBenchmark(problem):
+    if problem:
+        shutil.copy(problem, "./benchmark")
+    else:
+        with open('./benchmark', 'w') as benchmark:
+            benchmark.write(sys.stdin.read())
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("A script that wraps a podman call to a prover image")
     parser.add_argument("image_name", help="Image name e.g., eprover:3.0.03-runsolver-arm64")
-    parser.add_argument("--runscript", default="run PCWMI", help="The system script and its args, e.g., 'run_E PWI'")
     parser.add_argument("-P", "--problem", help="Problem file if not stdin")
+    parser.add_argument("--runscript", default="run_container PCWMI", help="The system script and its args, e.g., 'run_E PWI'")
     parser.add_argument("-C", "--cpu-limit", default=60, type=int, help="Max CPU time in seconds")
     parser.add_argument("-W", "--wall-clock-limit", default=60, type=int, help="Max wall clock time in seconds")
     parser.add_argument("-M", "--memory-limit", default=-1, type=int, help="Max memory usage in MB")
@@ -46,7 +53,7 @@ if __name__ == "__main__":
     if args.dry_run:
         print(command)
     else:
-        shutil.copy(args.problem, "./benchmark")
+        makeBenchmark(args.problem)
         subprocess.run(command, shell=True)
         os.remove("./benchmark")
     
